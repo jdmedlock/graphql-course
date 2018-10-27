7e3798cb6db786562199f9865c43314994a28cb7
jdmedlock <jdmedlock@gmail.com>
2018-10-27 13:44:22 -0500

    Feature: Add first demo in index.js and graphql-yoga module
    Add first demo in index.js and graphql-yoga module
    
    Resolves: N/a
    See also: N/a

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,1 +1,27 @@
-console.log('Hello GraphQL');
+import { GraphQLServer } from 'graphql-yoga';
+
+// GraphQL Type Definitions (schema)
+const typeDefs = `
+  type Query {
+    hello: String!
+  }
+`;
+
+// GraphQL Resolvers
+const resolvers = {
+  Query: {
+    hello() {
+      return 'Hello';
+    }
+  }
+};
+
+//
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
+
+server.start(() => {
+  console.log('The server is up and running!');
+});
