5829be51b1fdf89523cf64b447539898305b7a3b
jdmedlock <jdmedlock@gmail.com>
2018-10-28 09:45:19 -0500

    Feature: Update class examples
    Update class examples
    
    Resolves: N/a
    See also: N/a

--- graphql-basics/src/index.js
+++ graphql-basics/src/index.js
@@ -1,9 +1,28 @@
 import { GraphQLServer } from 'graphql-yoga';
 
+// Test data - Users
+const users = [{
+  id: '1',
+  name: 'Jim',
+  email: 'jim@me.com',
+  age: 63
+}, {
+  id: '2',
+  name: 'Joey',
+  email: 'joey@me.com',
+  age: 24
+}, {
+  id: '3',
+  name: 'Kay',
+  email: 'kay@me.com'
+}];
+
 // GraphQL Type Definitions (schema)
 const typeDefs = `
   type Query {
+    me: User!
     post: Post!
+    users: [User!]!
   }
 
   type Post {
@@ -12,11 +31,25 @@ const typeDefs = `
     body: String!
     published: Boolean!
   }
+
+  type User {
+    id: ID!
+    name: String!
+    email: String!
+    age: Int
+  }
 `;
 
 // GraphQL Resolvers
 const resolvers = {
   Query: {
+    me() {
+      return {
+        id: '123ABD',
+        name: 'John Doe',
+        email: 'jdoe@me.com',
+      };
+    },
     post() {
       return {
         id: '123ABD',
@@ -24,6 +57,9 @@ const resolvers = {
         body: 'Love this band',
         published: false
       };
+    },
+    users(parent, args, ctx, info) {
+      return users;
     }
   }
 };
